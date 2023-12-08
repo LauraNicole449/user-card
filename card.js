5588d84267d21fe14e115bc67fe32a6aab96c921
LauraNicole449 <lauranicolegarcia449@gmail.com>
2023-12-08 20:59:43 +0100

    added banner, avatar, name and description options

--- card.js
+++ card.js
@@ -1,15 +1,32 @@
-function ChangeName() {
-
-    let texto = document.getElementById("change-name").value;
-
-    document.getElementById("name").textContent = texto;
-
-    
+function changeName() {
+    let text = document.getElementById("change-name").value;
+    document.getElementById("name").textContent = text;
 }
-function subirImagen() {
-    // Obtener el input de tipo file
-    let input = document.getElementById("banner");
-    input.src = " ";
+function uploadBanner() {
+    let banner = document.getElementById("banner");
+    let chgbanner = document.getElementById("change-banner");
+        //obtain name and change banner
+      if (chgbanner.files && chgbanner.files[0]) {
+        let file = chgbanner.files[0];
+        let fileName = file.name;
+        banner.src = file.name;
+        console.log("Nombre del archivo:", fileName);
+    }
+}
+function uploadAvatar() {
+    let avatar = document.getElementById("avatar");
+    let chgavatar = document.getElementById("change-avatar");
+        //obtain name and change avatar
+      if (chgavatar.files && chgavatar.files[0]) {
+        let file = chgavatar.files[0];
+        let fileName = file.name;
+        avatar.src = file.name;
+        console.log("Nombre del archivo:", fileName);
+    }
+}
+function changeDescription(){
+    let text = document.getElementById("change-description").value;
+    document.getElementById("description").textContent = text;
     
-   
+  
 }
